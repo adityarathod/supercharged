437c0afff2c018c8e19f5bb77e329c5abb66a59e
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-08-18 17:21:13 -0700

    Switch over to better-sqlite3, other minor changes (#5)

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryblue: 'rgb(0, 122, 255)'
+      }
+    },
   },
   variants: {
     extend: {},
